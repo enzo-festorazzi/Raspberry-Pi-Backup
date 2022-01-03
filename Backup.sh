774e452b4d5ff70d9ad168b69120de25569c3e29
Enzo Festorazzi <31445489+enzo-festorazzi@users.noreply.github.com>
2022-01-03 13:02:17 -0300

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -10,7 +10,7 @@ BACKUP_DATO=$(date +%d-%m-%Y)
 dd if=/dev/mmcblk0 of=${BACKUP_PATH}/${BACKUP_NAME}.${BACKUP_DATO}.img bs=1MB
 
 # PiShrink
-pishrink.sh ${BACKUP_PATH}/${BACKUP_NAME}.${BACKUP_DATO}.img
+sudo pishrink.sh ${BACKUP_PATH}/${BACKUP_NAME}.${BACKUP_DATO}.img
 
 # Delete if there are more than ${BACKUP_ANTAL}
 pushd ${BACKUP_PATH}; ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n -${BACKUP_ANTAL} | xargs rm; popd
